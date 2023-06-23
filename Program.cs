902e88ff13f39b94cda0d2ac408c32bb0a9e97cf
NkosiDaniel <126275813+NkosiDaniel@users.noreply.github.com>
2023-06-22 21:05:45 -0400

    Update Program

--- Program.cs
+++ Program.cs
@@ -1,16 +1,16 @@
-﻿
-public class Multiples
+﻿public class Multiples
 {
     private static int sum;
 
     static void Main(string[] args)
     {
-        int baseCase = 0;
-        SumOfMultiples(baseCase);
+        
+        int num = 0;
+        SumOfMultiples(num);
     }
 
     private static void SumOfMultiples (int num)
-    {
+    {    //This is the base case of the recursive function. Due to its simplicity, it can be solved without a recursive call.
         if(num % 3 == 0 || num % 5 == 0)
         {
             sum += num;
@@ -19,8 +19,12 @@ public class Multiples
         {
             Console.WriteLine(sum);
             return;
+            //This will break the recursion
         }
         SumOfMultiples(num + 1);
+        //This is the recursive case. This involves reducing the overall problem to a simpler one so it can be solved recursively.
+        //In this case, we're adding 1 for every function call. Therefore, the problem gets simpler and simpler with each call
+        //because we get closer to 999
     }
 }
 
